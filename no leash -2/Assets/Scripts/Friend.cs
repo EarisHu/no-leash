904e6269db819a449cf1c3aa888b2b9ff2f1ad69
yerunying <153093233+yerunying@users.noreply.github.com>
2025-07-11 17:36:10 +0800

    Friend need to change

--- no leash -2/Assets/Scripts/Friend.cs
+++ no leash -2/Assets/Scripts/Friend.cs
@@ -6,25 +6,31 @@ public class Friend : MonoBehaviour
 {
     public GameObject dog;
     private bool following;
+    public float patrolLeftX;
+    public float patrolRightX;
+    public float speed;
+    private float scale;
 
     // Start is called before the first frame update
     void Start()
     {
         dog = GameObject.Find("dog");
         following = false;
+        speed = 10f;
+        patrolLeftX = 337.2f;
+        patrolRightX = 490.9f;
+        scale = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (following) ;
-        else
-        {
-
-        }
+        else Patrol();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
+    // IsTrigger = false
     {
         if (collision.gameObject == dog)
         {
@@ -32,5 +38,18 @@ public class Friend : MonoBehaviour
             transform.SetParent(dog.transform);
             following = true;
         }
+    }
+
+    void Patrol()
+    {
+        // Target(left<->right)
+        float targetX = Mathf.PingPong(Time.time * speed, patrolRightX - patrolLeftX) + patrolLeftX;
+        float direction = targetX - transform.position.x;
+        float moveDirection = Mathf.Sign(direction); // -1, 0, 1
+        rb.velocity = new Vector2(moveDirection * speed, rb.velocity.y);
+        // Change facing
+        if (moveDirection > 0) transform.localScale = new Vector3(1, 1, 1) * scale;
+        else if (moveDirection < 0)
+            transform.localScale = new Vector3(-1, 1, 1) * scale;
     }
 }
