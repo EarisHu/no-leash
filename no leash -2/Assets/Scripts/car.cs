baaffca946853249211be967d5a47c1f0f82debd
胡耘宁 <12311047@mail.sustech.edu.cn>
2025-07-15 15:56:20 +0800

    15：56

--- no leash -2/Assets/Scripts/car.cs
+++ no leash -2/Assets/Scripts/car.cs
@@ -5,25 +5,9 @@ using UnityEngine;
 public class car : MonoBehaviour
 {
     public GameObject dog;
-    public float speed = 50f;
     void Start()
     {
         dog = GameObject.FindWithTag("Dog");
-    }
-
-    void Update()
-    {
-        if (dog.transform == null) return;
-
-        Vector3 direction = (dog.transform.position - transform.position).normalized;
-
-        transform.position += direction * speed * Time.deltaTime;
-
-        Vector3 scale = transform.localScale;
-        scale.x = dog.transform.position.x < transform.position.x ? -Mathf.Abs(scale.x) : Mathf.Abs(scale.x);
-        transform.localScale = scale;
-
-
     }
     
     void OnTriggerEnter2D(Collider2D other)
