b76da032df90787cabf00e2b9ae579fe1f8cca66
胡耘宁 <12311047@mail.sustech.edu.cn>
2025-07-15 14:27:24 +0800

    pause

--- no leash -2/Assets/Scripts/home_1/DogController1.cs
+++ no leash -2/Assets/Scripts/home_1/DogController1.cs
@@ -28,7 +28,7 @@ public class DogController1 : MonoBehaviour
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         BoxCollider2D box = GetComponent<BoxCollider2D>();
-        groundCheckDistance = box.size.y * 0.5f * Mathf.Abs(transform.localScale.y) + 0.05f;
+        groundCheckDistance = box.size.y * 0.5f * Mathf.Abs(transform.localScale.y) + 1f;
     }
 
     void Update()
@@ -148,11 +148,12 @@ public class DogController1 : MonoBehaviour
 
         RaycastHit2D leftHit = Physics2D.Raycast(leftOrigin, Vector2.down, groundCheckDistance, groundLayer);
         RaycastHit2D rightHit = Physics2D.Raycast(rightOrigin, Vector2.down, groundCheckDistance, groundLayer);
+        RaycastHit2D centerHit = Physics2D.Raycast(origin, Vector2.down, groundCheckDistance, groundLayer);
 
         Debug.DrawRay(leftOrigin, Vector2.down * groundCheckDistance, leftHit.collider ? Color.green : Color.red);
         Debug.DrawRay(rightOrigin, Vector2.down * groundCheckDistance, rightHit.collider ? Color.green : Color.red);
 
-        return leftHit.collider != null || rightHit.collider != null;
+        return leftHit.collider != null || rightHit.collider != null || centerHit.collider != null;
     }
 
     void BloodChange()
