5348aad5c525acdd2f3fea62158c150ff4751dd0
yerunying <153093233+yerunying@users.noreply.github.com>
2025-07-17 18:54:46 +0800

    Countdown OK

--- no leash -2/Assets/hell/Countdown.cs
+++ no leash -2/Assets/hell/Countdown.cs
@@ -25,18 +25,19 @@ public class Countdown : MonoBehaviour
     {
         while (currentTime > 0)
         {
-            countdownText.text = Mathf.Ceil(currentTime).ToString();
+            int minutes = Mathf.FloorToInt(currentTime / 60f);
+            int seconds = Mathf.FloorToInt(currentTime % 60f);
+            countdownText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
             yield return new WaitForSeconds(1f);
             currentTime -= 1f;
         }
 
-        countdownText.text = "0";
+        countdownText.text = "00:00";
 
         // 倒计时结束后做的事：
         dog_hell dogScript = dog.GetComponent<dog_hell>();
         dogScript.Die();
         Debug.Log("Time's up!");
-        // SceneManager.LoadScene("NextSceneName");
-        // 或者 触发事件等
     }
 }
